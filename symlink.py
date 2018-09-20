fb87c5ab1e73ed93d8bf931815064949c32edb08
Ruslan Osipov <ruslan@rosipov.com>
2018-09-20 15:50:15 -0700

    Port symlink.py to cross-platform Python 3

--- symlink.py
+++ symlink.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 ##############################################################################
 # Automatically symlink all dotfiles to the user's home directory.
 #
 # Copyright (c) 2013, Martey Dodoo.
+#
+# This has been made cross-platform and ported to Python 3 by Ruslan Osipov.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -10,10 +13,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,14 +25,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 ##############################################################################
+
 import errno
 import os
 import shutil
+import sys
 
+IS_WINDOWS = sys.platform == 'win32'
 
-home_dir = os.environ["HOME"] + '/'
+if IS_WINDOWS:
+    HOME_ENV = 'USERPROFILE'
+    DOTFILE_PREFIX = '_'
+else:
+    HOME_ENV = 'HOME'
+    DOTFILE_PREFIX = '.'
+
+home_dir = os.environ[HOME_ENV]
 # Name of the directory where dotfiles are located.
-dotfiles_dir = home_dir + ".dotfiles/"
+dotfiles_dir = os.path.join(home_dir, DOTFILE_PREFIX + "dotfiles")
 # List of things we should ignore in the dotfiles directory.
 ignore = [
     ".git",
@@ -40,15 +53,15 @@ ignore = [
     ".gitmodules"
 ]
 # Name of the directory where already-existing dotfiles should be moved.
-backup_dir = home_dir + "dotfiles-backup/"
+backup_dir = os.path.join(home_dir, "dotfiles-backup")
 
 # Create backup directory if needed.
 try:
     os.mkdir(backup_dir)
-    print "Backup directory %s created." % backup_dir
+    print("Backup directory %s created." % backup_dir)
 except OSError as backup_creation_err:
     if backup_creation_err.errno == errno.EEXIST:
-        print "Backup directory %s already exists." % backup_dir
+        print("Backup directory %s already exists." % backup_dir)
     else:
         raise
 
@@ -61,27 +74,35 @@ for filename in dotfiles:
         continue
 
     # Add dots to dotfile names.
-    dotfile = '.' + filename
+    if IS_WINDOWS and filename == 'vim':  # .vim turns into vimfile in Windows
+        dotfile = 'vimfiles'
+    else:
+        dotfile = DOTFILE_PREFIX + filename
 
     # Assume that this is a directory and try to create a symlink.
     try:
-        os.symlink(dotfiles_dir + filename, home_dir + dotfile)
-        print "Linked %s to %s." % (dotfiles_dir + dotfile, home_dir + dotfile)
+        os.symlink(os.path.join(dotfiles_dir, filename),
+                   os.path.join(home_dir, dotfile))
+        print("Linked %s to %s." % (os.path.join(dotfiles_dir, dotfile),
+                                    os.path.join(home_dir, dotfile)))
     except OSError as link_err:
         if link_err.errno == errno.EEXIST:
             # Check to see if this is a symlink, which means it has
             # already been copied.
-            if os.path.islink(home_dir + dotfile):
-                print "%s is already a symlink." % (home_dir + dotfile)
+            if os.path.islink(os.path.join(home_dir, dotfile)):
+                print("%s is already a symlink." % os.path.join(
+                        home_dir, dotfile))
                 continue
 
             # This file already exists in the home directory,
             # so we move the old file to the backup directory.
-            print "%s already exists in %s." % (dotfile, home_dir)
-            shutil.move(home_dir + dotfile, backup_dir + dotfile)
-            print "Moved %s to %s" % (dotfile, backup_dir)
-            os.symlink(dotfiles_dir + dotfile, home_dir + dotfile)
-            print "Linked %s to %s." % (dotfiles_dir + filename,
-                                        home_dir + dotfile)
+            print("%s already exists in %s." % (dotfile, home_dir))
+            shutil.move(os.path.join(home_dir, dotfile),
+                        os.path.join(backup_dir, dotfile))
+            print("Moved %s to %s" % (dotfile, backup_dir))
+            os.symlink(os.path.join(dotfiles_dir, dotfile),
+                       os.path.join(home_dir, dotfile))
+            print("Linked %s to %s." % (os.path.join(dotfiles_dir, filename),
+                                        os.path.join(home_dir, dotfile)))
         else:
             raise
