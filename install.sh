19cc92252f645577b062e881c0a8706f734b68c4
Ruslan Osipov <ruslan@rosipov.com>
2018-10-02 11:22:18 -0700

    Add -y to install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python3 symlink.py
 source $HOME/.bashrc
 touch $HOME/.gitconfig.local
-sudo apt-get install git vim ack-grep python-pip tree ranger
+sudo apt-get install git vim ack-grep python-pip tree ranger -y
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 pip install --user thefuck virtualenvwrapper
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
