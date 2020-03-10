63975e4c3cce61d231613579173baaf05ac8bdc1
Ruslan Osipov <ruslan@rosipov.com>
2020-03-10 09:12:26 -0700

    Remove thefuck from the install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 python3 symlink.py
 source $HOME/.bashrc
 touch $HOME/.gitconfig.local
-sudo apt install git vim ack-grep python3 tree ranger thefuck -y
+sudo apt install git vim ack-grep python3 tree ranger -y
 sudo dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
 python3 -m pip install --user virtualenvwrapper
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
