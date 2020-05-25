5698617f2cb01ffa5bac928bd10f5146b317876f
Ruslan Osipov <ruslan@rosipov.com>
2020-05-25 19:47:20 +0000

    Update sync.sh to work with Drive

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd $HOME/Dropbox/blog/img/input
-mogrify -resize x620 -quality 100 -path $HOME/Dropbox/blog/img/output *
-exiftool -all= $HOME/Dropbox/blog/img/output/*
-cp $HOME/Dropbox/blog/posts/* $HOME/blog/source/_posts
-cp $HOME/Dropbox/blog/img/output/* $HOME/blog/source/images/posts
+rclone sync googledrive:blog drive_blog
+cd $HOME/drive_blog/img/input
+mogrify -resize x620 -quality 100 -path $HOME/drive_blog/img/output *
+exiftool -all= $HOME/drive_blog/img/output/*
+cp $HOME/drive_blog/posts/* $HOME/blog/source/_posts
+cp $HOME/drive_blog/img/output/* $HOME/blog/source/images/posts
 cd $HOME/blog
 rake generate
 rake preview
